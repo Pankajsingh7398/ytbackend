85b41bb24ece88cdf151689be7aeec3d33e057ca
Pankaj Singh <pankajmall7398@gmail.com>
2026-09-17 12:00:08 +0530

    subscription model add

---
+++ src/models/subscription.model.js
@@ -0,0 +1,16 @@
+import { Timestamp } from "mongodb";
+import Mongoose,{schema } from "mongoose";
+const subscriptionSchema = new schema({
+    subscriber:{
+        type:schema.Types.ObjectId,
+        ref:"User"
+    },
+    channel:{
+        type:schema.Types.ObjectId,
+        ref:"User"
+    }
+
+},{
+timestamps:true
+})
+export const subscription=Mongoose.model(subscription,subscriptionSchema)
